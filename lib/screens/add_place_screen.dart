1423bb235bdf64c95ebab1e851e835b1b540bbc6
truncet <theonlyeminem@gmail.com>
2021-05-05 21:33:42 +0545

    camera added

--- lib/screens/add_place_screen.dart
+++ lib/screens/add_place_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/image_input.dart';
+
 class AddPlaceScreen extends StatefulWidget {
   static const routeName = '/add-place';
   @override
@@ -7,6 +9,7 @@ class AddPlaceScreen extends StatefulWidget {
 }
 
 class _AddPlaceScreenState extends State<AddPlaceScreen> {
+  final _titleController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,13 +17,38 @@ class _AddPlaceScreenState extends State<AddPlaceScreen> {
         title: Text('Add a New Place'),
       ),
       body: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          Text('User Inputs'),
+          Flexible(
+              child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Column(
+              children: [
+                TextField(
+                  decoration: InputDecoration(
+                    labelText: 'Title',
+                  ),
+                  controller: _titleController,
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                ImageInput(),
+              ],
+            ),
+          )),
           ElevatedButton.icon(
-            onPressed: () {},
-            icon: Icon(Icons.add),
-            label: Text('Add Place'),
-          ),
+              onPressed: () {},
+              icon: Icon(Icons.add),
+              label: Text('Add Place'),
+              style: ButtonStyle(
+                elevation: MaterialStateProperty.all<double>(0),
+                tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                backgroundColor: MaterialStateProperty.all<Color>(
+                  Theme.of(context).accentColor,
+                ),
+              )),
         ],
       ),
     );
---
+++ lib/widgets/image_input.dart
@@ -0,0 +1,60 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class ImageInput extends StatefulWidget {
+  @override
+  _ImageInputState createState() => _ImageInputState();
+}
+
+class _ImageInputState extends State<ImageInput> {
+  File _storedImage;
+
+  Future<void> _takePicture() async {
+    final imageInst = ImagePicker();
+    final imageFile =
+        await imageInst.getImage(source: ImageSource.camera, maxWidth: 600);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Container(
+          width: 150,
+          height: 150,
+          decoration: BoxDecoration(
+            border: Border.all(width: 1, color: Colors.grey),
+          ),
+          child: _storedImage != null
+              ? Image.file(
+                  _storedImage,
+                  fit: BoxFit.cover,
+                  width: double.infinity,
+                )
+              : Text(
+                  'No Image Taken',
+                  textAlign: TextAlign.center,
+                ),
+          alignment: Alignment.center,
+        ),
+        SizedBox(
+          width: 10,
+        ),
+        Expanded(
+          child: TextButton.icon(
+            onPressed: _takePicture,
+            icon: Icon(Icons.camera),
+            label: Text('Take Picture'),
+            style: ButtonStyle(
+              foregroundColor: MaterialStateProperty.all<Color>(
+                Theme.of(context).primaryColor,
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
